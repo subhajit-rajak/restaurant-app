87ad453d5f9c574e37bdaef5296f34c2d3faa279
Subhajit Rajak <subhajitrajak2001@gmail.com>
2024-08-08 13:25:12 +0530

    Added feature for issue #4 - Bottom Navigation

--- app/src/main/java/com/gloomdev/restaurantapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/gloomdev/restaurantapp/ui/activities/MainActivity.kt
@@ -1,20 +1,29 @@
-package com.gloomdev.restaurantapp
+package com.gloomdev.restaurantapp.ui.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.gloomdev.restaurantapp.R
+import com.gloomdev.restaurantapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val navController = findNavController(R.id.fragmentContainerView)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
