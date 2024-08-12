9279711dd819e58d626029ae59841e0773bab5b4
Deepak Choudhary <deepakkaler551@gmail.com>
2024-08-12 18:25:38 +0530

    Add Profile Fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,5 +52,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //    GLIDE
     implementation (libs.glide)
+
+    // Rounded Image View
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
