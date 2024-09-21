d12bcbb12c64950120c178180a7a1709195d06f5
Subhajit Rajak <subhajitrajak2001@gmail.com>
2024-09-21 13:36:19 +0530

    UI upgrade

--- app/src/main/java/com/gloomdev/restaurantapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/gloomdev/restaurantapp/ui/activities/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(binding.root)
         FirebaseApp.initializeApp(this)
         val navController = findNavController(R.id.fragmentContainerView)
